f00ad27329df0811d4ea4fc59564bc4bf53cf8eb
Michael Yong <wrong92@gmail.com>
2021-12-22 10:47:52 -0500

    tsc

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 import Pikaday from "pikaday";
 
 function App() {
-  const containerEl = useRef(null);
+  const containerEl = useRef<HTMLDivElement>(null);
   useEffect(() => {
     var fromPicker = new Pikaday({
       onSelect: function (date) {
@@ -33,16 +33,17 @@ function App() {
       },
     });
 
-    if (window.defaultDateFrom) {
-      fromPicker.setDate(window.defaultDateFrom);
+    const winAny = window as any;
+    if (winAny.defaultDateFrom) {
+      fromPicker.setDate(winAny.defaultDateFrom);
     }
-    if (window.defaultDateTo) {
-      toPicker.setDate(window.defaultDateTo);
+    if (winAny.defaultDateTo) {
+      toPicker.setDate(winAny.defaultDateTo);
     }
-
-
-    containerEl.current.appendChild(fromPicker.el);
-    containerEl.current.appendChild(toPicker.el);
+    if (containerEl.current) {
+      containerEl.current.appendChild(fromPicker.el);
+      containerEl.current.appendChild(toPicker.el);
+    }
   }, []);
 
   return <div className="App" ref={containerEl}></div>;
